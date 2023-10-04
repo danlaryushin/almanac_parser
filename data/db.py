8bbc96e25e04166d9a44f40ee18fd1f0538dfc42
danlaryushin <danlaryushin@gmail.com>
2023-10-04 14:23:15 +0300

    fix db.py

--- data/db.py
+++ data/db.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-#from data.models import Base, Basket
-from models import Base, Basket
+from data.models import Base, Basket
 engine = create_engine('sqlite:///basket.db', echo=False)
 Base.metadata.create_all(engine)
 session = Session(engine)
